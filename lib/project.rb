14d6a061102b33b104020ab773ed03e67f580116
Alexander Spanos <alexisspa9@gmail.com>
2018-03-26 14:50:54 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,6 +2,10 @@ class Project
   attr_accessor :title, :backers
   def initialize(title)
     @title = title
-@backers = []
+    @backers = []
+  end
+  def add_backer(backer)
+    @backers << backer
+    
   end
 end
